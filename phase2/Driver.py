3910c17499e0d513a03f985725458d5038032325
Magnus Simoni Jahn <95537506+magnusjahn@users.noreply.github.com>
2025-12-03 18:10:14 +0100

    Added methods for Driver class

--- phase2/Driver.py
+++ phase2/Driver.py
@@ -1,16 +1,21 @@
-from phase2.Point import Point
-from phase2.Request import Request
-from phase2.behaviour.DriverBehaviour import DriverBehaviour
+from __future__ import annotations
+
+import math
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from phase2.Point import Point
+    from phase2.Request import Request
+    from phase2.behaviour.DriverBehaviour import DriverBehaviour
 from enum import Enum
 
 class DriverStatus(Enum):
     IDLE = 1,
     TO_PICKUP = 2
-    FROM_DROPOFF = 3
+    TO_DROPOFF = 3
 
 class Driver:
-    def __init__(self, id: int, position: Point, speed: float, status: DriverStatus, current_request: Request | None,
-                 behaviour: DriverBehaviour, history: list) -> None:
+    def __init__(self, id: int, position: 'Point', speed: float, status: DriverStatus, current_request: "'Request' | None",
+                 behaviour: 'DriverBehaviour', history: list) -> None:
         self.id = id
         self.position = position
         self.speed = speed
@@ -18,33 +23,61 @@ class Driver:
         self.current_request = current_request
         self.behaviour = behaviour
         self.history = history
+        self.dir_vector = None
+
+    def direction_vector(self) -> None:
+        if self.target_point() is not None:
+            x, y = self.position.x - self.target_point().x, self.position.y - self.target_point().y
+            magnitude = math.sqrt(x ** 2 + y ** 2)
+            x_normalized, y_normalized = (x / magnitude, y / magnitude)
+            self.dir_vector = (x_normalized, y_normalized)
+        else:
+            self.dir_vector = None
+
 
     def assign_request(self, request: Request, current_time: int) -> None:
-        pass
+        # Do we need to implement assignment based on behaviour here???
+        self.current_request = request
+        self.status = DriverStatus.TO_PICKUP
+        self.current_request.mark_assigned(self.id)
+        self.direction_vector()
+        # TO-DO: Implement data collection
 
     def target_point(self) -> Point | None:
         """
         Returns:
             The next target. Pickup point or dropoff point depending on DriverStatus.
         """
-        pass
+        if self.status == DriverStatus.IDLE:
+            return None
+        elif self.status == DriverStatus.TO_PICKUP:
+            return self.current_request.pick_up
+        elif self.status == DriverStatus.TO_DROPOFF:
+            return self.current_request.drop_off
+        return None
 
     def step(self, dt: float) -> None:
         """
         Moves the driver towards the current target according to speed and time step dt.
         """
-        pass
+        self.position.x += self.dir_vector.x * self.speed * dt
+        self.position.y += self.dir_vector.y * self.speed * dt
 
     def complete_pickup(self, time: int) -> None:
         """
         Updates the internal state when the pickup is reached.
         """
-        pass
-
+        self.status = DriverStatus.TO_DROPOFF
+        self.current_request.mark_picked(time)
+        self.direction_vector()
+        # TO-DO: Implement data collection
 
     def complete_dropoff(self, time: int) -> None:
         """
         Updates the internal state and history when the dropoff is reached.
         """
-        pass
+        self.status = DriverStatus.IDLE
+        self.current_request.mark_delivered(time)
+        self.direction_vector()
+        # TO-DO: Implement data collection.
 
