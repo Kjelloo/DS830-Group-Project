f111f23af291173df3908fac3711b4845564f5d9
Magnus Simoni Jahn <95537506+magnusjahn@users.noreply.github.com>
2025-12-09 13:40:18 +0100

    Added preliminary decide method for GreedyDistanceBehaviour

--- phase2/behaviour/GreedyDistanceBehaviour.py
+++ phase2/behaviour/GreedyDistanceBehaviour.py
@@ -2,16 +2,34 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from behaviour.DriverBehaviour import DriverBehaviour
+from DriverBehaviour import DriverBehaviour
 
 if TYPE_CHECKING:
-    from Driver import Driver
-    from Offer import Offer
+    from ..Driver import Driver
+    from ..Offer import Offer
 
 
 class GreedyDistanceBehaviour(DriverBehaviour):
-    def decide(self, driver: Driver, offer: Offer, time: int):
+    def decide(self, driver: Driver, offer: Offer, time: int) -> bool:
         """
         Accept if the distance to the pickup is below a given threshold.
         """
-        pass
+
+        # Set the threshold for the quotient of distance to the pickup point / total
+        # distance the driver can travel in x ticks. If the quotient is larger than
+        # that, return False, else return True.
+        threshold = 0.33
+
+        # Fix x: the amount of ticks the quotient will be based on.
+        x = 30
+
+        # Get distance driver can travel in x ticks.
+        traversable_distance = driver.speed * x
+
+        # Get distance to pickup point.
+        dist_to_pickup = driver.position.distance_to(offer.request.pickup)
+
+        if (dist_to_pickup / traversable_distance) < threshold:
+            return True
+        else:
+            return False
