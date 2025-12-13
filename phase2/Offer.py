28860bfad49cdebbaff4378c557b35c64bdae725
Magnus Simoni Jahn <95537506+magnusjahn@users.noreply.github.com>
2025-12-13 12:51:36 +0100

    unittesting for offer added

--- phase2/Offer.py
+++ phase2/Offer.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from Driver import Driver
-from Request import Request
+from phase2.Driver import Driver
+from phase2.Request import Request
 
 
 @dataclass
@@ -12,3 +12,13 @@ class Offer:
     request: Request
     estimated_travel_time: float
     estimated_reward: float
+
+    def __post_init__(self):
+        if not isinstance(self.driver, Driver):
+            raise TypeError("driver must be a Driver")
+        if not isinstance(self.request, Request):
+            raise TypeError("request must be a Request")
+        if not isinstance(self.estimated_travel_time, (int, float)):
+            raise TypeError("estimated_travel_time must be a number")
+        if not isinstance(self.estimated_reward, (int, float)):
+            raise TypeError("estimated_reward must be a number")
