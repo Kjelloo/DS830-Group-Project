adc782b4490655fe1474c26fb7d4bfbd40670dff
Magnus Simoni Jahn <95537506+magnusjahn@users.noreply.github.com>
2025-12-16 00:19:53 +0100

    merged NearestNeigh.. with amiras version

--- phase2/dispatch/NearestNeighborPolicy.py
+++ phase2/dispatch/NearestNeighborPolicy.py
@@ -9,7 +9,8 @@ class NearestNeighborPolicy(DispatchPolicy):
     def assign(self, drivers: list[Driver], requests: list[Request], time: int, run_id: str) -> list[
         tuple[Driver, Request]]:
         """
-        Repeatedly match a driver to the closest two requests.
+        repeatedly match the closest idle driver to the closest waiting request,
+        avoiding reuse of drivers and requests
         """
         # Get list of idle drivers and waiting requests
         idle_drivers = [driver for driver in drivers if driver.status.value == DriverStatus.IDLE.value]
@@ -19,23 +20,27 @@ class NearestNeighborPolicy(DispatchPolicy):
         if len(idle_drivers) == 0 or len(waiting_requests) == 0:
             return []
 
-        # Create empty list to store pairs
+        # Initialize list for holding matched pairs
         pairs = []
+        # Initialize set for quick look-up speed for already assigned requests
         assigned_requests = set()
 
-        # Iteratively match the next driver to the closest
+        # Repeatedly find the closest (driver, request) pair and remove them from the pool
         for driver in idle_drivers:
             current_request = None
             current_distance = 999 # arbitrarily high number
 
+            # Find the closest pair among remaining idle drivers and waiting requests
             for request in waiting_requests:
                     if request.id not in assigned_requests:
-                        distance = driver.position.distance_to(request.pickup) + request.pickup.distance_to(request.dropoff)
+                        distance = driver.position.distance_to(request.pickup)
                         if distance < current_distance:
                             current_distance = distance
                             current_request = request
-                            assigned_requests.add(request.id)
+
+            # Append new pair if it exists
             if current_request is not None:
+                assigned_requests.add(current_request.id)
                 pairs.append((driver, current_request))
 
         return pairs
